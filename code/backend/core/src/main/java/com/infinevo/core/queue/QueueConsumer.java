7a2643e9e683f6d0dc504bb3dcbb58da44e0acfb
Sayeed <sahmad@infinevocloud.com>
2026-09-25 17:53:01 +0530

    feat: W-53.1 cache cleanup - remove dead core cache and queue classes

--- code/backend/core/src/main/java/com/infinevo/core/queue/QueueConsumer.java
+++
@@ -1,7 +0,0 @@
-package com.infinevo.core.queue;
-
-/**
- * Interface for consuming background jobs from queues.
- * Inherits from com.infinevo.shared.queue.QueueConsumer.
- */
-public interface QueueConsumer<T> extends com.infinevo.shared.queue.QueueConsumer<T> {}
